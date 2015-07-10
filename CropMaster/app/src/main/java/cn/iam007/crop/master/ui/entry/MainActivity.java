6236fa59d62839e6e82522691dacfc724dbeff32
jiangerji <jiangerji@gmail.com>
2015-07-10 17:19:00 +0800

    1.  增加调整图片边界界面和最后预览界面 2.  增加引导页 3.  加入leancloud依赖，增加版本更新检查

--- CropMaster/app/src/main/java/cn/iam007/crop/master/ui/entry/MainActivity.java
+++ CropMaster/app/src/main/java/cn/iam007/crop/master/ui/entry/MainActivity.java
@@ -2,9 +2,13 @@ package cn.iam007.crop.master.ui.entry;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 import cn.iam007.base.BaseActivity;
+import cn.iam007.base.Iam007Service;
+import cn.iam007.base.utils.LogUtil;
 import cn.iam007.crop.master.R;
 import cn.iam007.crop.master.ui.crop.CropActivity;
 import cn.iam007.mediapicker.MediaPickerBuilder;
@@ -29,6 +33,27 @@ public class MainActivity extends BaseActivity {
                 builder.showDialog();
             }
         });
+
+        // 启动检查更新
+        Intent intent = new Intent();
+        intent.setClass(this, Iam007Service.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mExitHintToast = null;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mExitHintToast != null) {
+            mExitHintToast.cancel();
+        }
     }
 
     @Override
@@ -50,5 +75,47 @@ public class MainActivity extends BaseActivity {
                     break;
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 关闭service
+        Intent intent = new Intent();
+        intent.setClass(this, Iam007Service.class);
+        stopService(intent);
+    }
+
+    // 上次按下返回键的时间
+    private long mPreBackPressedTS = 0;
+    private Toast mExitHintToast = null;
+
+    private Handler mToastHandler = new Handler();
+
+    @Override
+    public void onBackPressed() {
+        LogUtil.d("onBackPressed!");
+        long currentTS = System.currentTimeMillis();
+        if (currentTS - mPreBackPressedTS < 3000) {
+            super.onBackPressed();
+        }
+
+        if (mExitHintToast != null) {
+            mExitHintToast.cancel();
+        }
+        mExitHintToast = Toast.makeText(this, R.string.exit_hint, Toast.LENGTH_SHORT);
+        mExitHintToast.show();
+        mPreBackPressedTS = currentTS;
+
+        mToastHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mExitHintToast != null) {
+                    mExitHintToast.cancel();
+                    mExitHintToast = null;
+                }
+            }
+        }, 3000);
     }
 }
--- CropMaster/base/src/main/java/cn/iam007/base/BaseApplication.java
+++ CropMaster/base/src/main/java/cn/iam007/base/BaseApplication.java
@@ -2,7 +2,11 @@ package cn.iam007.base;
 
 import android.app.Application;
 
+import com.avos.avoscloud.AVOSCloud;
+
 import cn.iam007.base.utils.ImageUtils;
+import cn.iam007.base.utils.LogUtil;
+import cn.iam007.base.utils.PlatformUtils;
 import cn.iam007.base.utils.SharedPreferenceUtil;
 
 /**
@@ -16,6 +20,13 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mApplication = this;
+
+        String appId = PlatformUtils.getMeteDataByKey(this, "leancloud_app_id");
+        String appKey = PlatformUtils.getMeteDataByKey(this, "leancloud_app_key");
+        AVOSCloud.initialize(this, appId, appKey);
+        LogUtil.d("Init LeanCloud");
+        LogUtil.d("    App id:" + appId);
+        LogUtil.d("    App key:" + appKey);
 
         // 初始化显示图片配置，初始化image loader
         ImageUtils.init(this);
